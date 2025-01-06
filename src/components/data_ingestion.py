da70d5b8ee46a425a1fe1e822fac1e06acb07fe2
Nitu-git <yadavanita1177@gmail.com>
2025-01-05 22:51:44 -0600

    model trainer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 
 from data_transformation import DataTransformationConfig
 from data_transformation import DataTransformation
+from model_trainer import ModelTrainerConfig
+from model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -53,5 +55,7 @@ if __name__=="__main__":
     train_path, test_path = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
-
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+   
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,9 @@ import dill
 from src.exception import CustomException
 from src.logger import logging
 
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -17,4 +20,29 @@ def save_object(file_path, obj):
 
     except Exception as e:
         logging.info("excpetion occured while saving object")
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            logging.info(f"train model score: {train_model_score} for model : {model}")
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+            
+        logging.info(f"report from evaluate_models: {report}")
+        return report
+    except Exception as e:
+        logging.info("exception occurred while evaluating models")
         raise CustomException(e, sys)
